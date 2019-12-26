31b4a468c5032e3f964e74553eff40ef559b4753
Oli <55298769+xMistt@users.noreply.github.com>
2019-12-26 00:52:00 +0000

    Update fortnite.py

--- fortnite.py
+++ fortnite.py
@@ -1,6 +1,6 @@
 """
 “Commons Clause” License Condition v1.0
-Copyright Pushii 2019
+Copyright Oli 2019
 
 The Software is provided to you by the Licensor under the
 License, as defined below, subject to the following condition.
@@ -19,33 +19,7 @@ of the Software. Any license notice or attribution required by
 the License must also include this Commons Clause License
 Condition notice.
 
-Software: Push3rBot
-
-License: Apache 2.0
-"""
-
-"""
-“Commons Clause” License Condition v1.0
-Copyright Pushii 2019
-
-The Software is provided to you by the Licensor under the
-License, as defined below, subject to the following condition.
-
-Without limiting other conditions in the License, the grant
-of rights under the License will not include, and the License
-does not grant to you, the right to Sell the Software.
-
-For purposes of the foregoing, “Sell” means practicing any or
-all of the rights granted to you under the License to provide
-to third parties, for a fee or other consideration (including
-without limitation fees for hosting or consulting/ support
-services related to the Software), a product or service whose
-value derives, entirely or substantially, from the functionality
-of the Software. Any license notice or attribution required by
-the License must also include this Commons Clause License
-Condition notice.
-
-Software: Push3rBot
+Software: PartyBot
 
 License: Apache 2.0
 """
